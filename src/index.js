74ce43b040e5b9e99b1eecdeefe062ade375bb85
Karen <karen.dev999@gmail.com>
2020-01-23 20:21:29 +0300

    add eslint

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 export default () => {
     const name = readlineSync.question('May I have your name?');
-    return `Hello, ${name}`;
+    return 'Hello, ${name}';
 };
 
 
