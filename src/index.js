047d0ea596a352e37a25c668df1f65644898980c
karen9999 <karen.dev999@gmail.com>
2020-03-14 22:43:05 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -15,9 +15,10 @@ const playGame = (condition, getQuestionAndAnswer) => {
       console.log("Correct!");
     } else {
       console.log(
-        `"${answer}" is wrong answer ;(. Correct answer was "${correctAnswer}".`);
-        console.log(`Let's try again, ${name}!`);
-        return;
+        `"${answer}" is wrong answer ;(. Correct answer was "${correctAnswer}".`
+      );
+      console.log(`Let's try again, ${name}!`);
+      return;
     }
   }
   console.log(`Congratulations, ${name}!`);
